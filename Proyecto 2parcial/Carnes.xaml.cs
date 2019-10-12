50250c28970fc88c3cc9d9ca5778ca0e319d50cf
romiinavega <romiivega3005@gmail.com>
2019-10-12 12:49:34 -0700

    Agregar archivos de proyecto.

---
+++ Proyecto 2parcial/Carnes.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proyecto_2parcial
+{
+    /// <summary>
+    /// Lógica de interacción para Carnes.xaml
+    /// </summary>
+    public partial class Carnes : UserControl
+    {
+        public Carnes()
+        {
+            InitializeComponent();
+            restriccion.Visibility = Visibility.Hidden;
+        
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (infoingrediente1.Text == string.Empty || infoingrediente2.Text == string.Empty || infoingrediente3.Text == string.Empty || infonota.Text == string.Empty || infoqueso.Text == string.Empty || infotamaño.Text == string.Empty)
+            {
+                restriccion.Visibility = Visibility.Visible;
+            }
+
+            else
+            {
+                carnes.Children.Clear();
+
+            }
+        }
+
+        private void infoingrediente1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            restriccion.Visibility = Visibility.Hidden;
+        }
+
+        private void infoingrediente2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            restriccion.Visibility = Visibility.Hidden;
+        }
+
+        private void infoingrediente3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            restriccion.Visibility = Visibility.Hidden;
+        }
+
+        private void infonota_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            restriccion.Visibility = Visibility.Hidden;
+        }
+
+        private void infoqueso_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            restriccion.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            carnes.Children.Clear();
+        }
+    }
+}
---
+++ Proyecto 2parcial/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proyecto_2parcial
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Formulario.Children.Clear();
+        }
+
+        private void BtnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            switch (articulo.SelectedIndex)
+            {
+                case 0: 
+                   Formulario.Children.Add(new Pizza());
+                    break;
+
+                case 1:
+                    Formulario.Children.Add(new Hamburguesa());
+                    break;
+
+                case 2: 
+                    Formulario.Children.Add(new Sushi());
+                    break;
+                case 3: 
+                   Formulario.Children.Add(new Carnes());
+
+                    break;
+
+                case 4: 
+                    Formulario.Children.Add(new Ensaladas());
+        
+                    break;
+
+                case 5:
+                   Formulario.Children.Add(new Bebida());
+
+                    break;
+
+
+            }
+        }
+
+    }
+}
+
